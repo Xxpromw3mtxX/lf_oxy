3024cf360074a68344349689f5ba21eb8b2daaf3
Francesco Metta <fra.metta@hotmail.it>
2022-01-11 22:14:04 +0100

    started writing an initial client file

--- client/main.lua
+++ client/main.lua
@@ -7,10 +7,12 @@ local oxyPed
 local randomPed = 1
 local randomDelivery = 1
 local randomPedSeller = 1
+local randomPedDelivery = 1
 local hasStarted = false
 
 --items amounts
 local suspicious
+local recoveries
 local maxRewardOxy
 
 --blip
@@ -65,21 +67,42 @@ function setWaypoint(coords, bName)
 	EndTextCommandSetBlipName(oxyBlips)
 
 	SetBlipRoute(oxyBlips, true)
+
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = _U('gpsSetted'), length = 2500})
+end
+
+function generateDelivery()
+	randomDelivery = math.random(1, #Config.deliveryPoints)
+	randomPedDelivery = math.random(1, #Config.pedModels)
+
+	setWaypoint(Config.deliveryPoints[randomDelivery], _U('exchange'))
+	
 end
 
 RegisterNetEvent('atlantis_oxy:initOxy')
 AddEventHandler('atlantis_oxy:initOxy', function()
-	randomPed = math.random(1, #Config.npcLocations)
-    randomDelivery = math.random(1, #Config.deliveryPoints)
-	randomPedSeller = math.random(1, #Config.sellerPed)
+	if Config.usebMoney then
+		TriggerServerEvent('atlantis_oxy:clearMoney', Config.mAccount, Config.startAmount)
+	else
+		TriggerEvent('atlantis_oxy:startOxy')
+	end
+end)
 
+RegisterNetEvent('atlantis_oxy:startOxy')
+AddEventHandler('atlantis_oxy:startOxy', function()
+	randomPed = math.random(1, #Config.npcLocations)
+	randomPedSeller = math.random(1, #Config.sellerPed)
+	
 	suspicious = math.random(1, Config.maxStartItem)
+	recoveries = suspicious
 	maxRewardOxy = math.random(1, Config.maxOxy)
 
-	hasStarted = true
+	hasStarted = not hasStarted
 
-	createNPC(Config.sellerPed[randomPedSeller].type, Config.sellerPed[randomPedSeller].model, Config.npcLocations[randomPed].position, Config.npcLocations[randomPed].heading, false, true)
+	TriggerEvent('mythic_notify:client:SendAlert', {type = 'inform', text = _U('oxyHasStarted'), length = 2500})
+
 	setWaypoint(Config.npcLocations[randomPed].position, _U('oRecovery'))
+	createNPC(Config.sellerPed[randomPedSeller].type, Config.sellerPed[randomPedSeller].model, Config.npcLocations[randomPed].position, Config.npcLocations[randomPed].heading, false, true)
 end)
 
 Citizen.CreateThread(function()
@@ -87,9 +110,42 @@ Citizen.CreateThread(function()
 		Citizen.Wait(Config.TickTime)
 
 		local coords = GetEntityCoords(GetPlayerPed(-1))
+		local pDistance = GetDistanceBetweenCoords(coords, Config.npcLocations[randomPed].position, false)
 		
-		if hasStarted and GetDistanceBetweenCoords(coords, Config.npcLocations[randomPed].position, false) < 10 then
+		if hasStarted and pDistance < 10 then
 			RemoveBlip(oxyBlips)
 		end
+
+		if hasStarted and suspicious ~= 0 then
+			exports['qtarget']:AddTargetModel({Config.sellerPed[randomPedSeller].model}, {
+				options = {
+					{
+						event = "atlantis_oxy:addPackage",
+						icon = "fas fa-box",
+						label = _U('recoverPacket'),
+						num = 1
+					}
+				},
+				distance = Config.interactDistances
+			})
+		end
+
+		if hasStarted and suspicious == 0 then
+			exports.qtarget:RemoveTargetModel({Config.sellerPed[randomPedSeller].model}, {
+				_U('recoverPacket')
+			})
+			Citizen.Wait(60000)
+			removeNPC()
+		end
+
+		if hasStarted and recoveries ~= 0 and suspicious == 0 then
+			generateDelivery()
+		end
 	end
+end)
+
+AddEventHandler('atlantis_oxy:addPackage', function()
+	suspicious = suspicious - 1
+
+	TriggerServerEvent('atlantis_oxy:itemAdder', Config.startItem)
 end)
