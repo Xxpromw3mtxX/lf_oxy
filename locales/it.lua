d53df80894df86b970c02c6c9c11c9b7ab361582
Francesco Metta <fra.metta@hotmail.it>
2022-01-11 22:13:44 +0100

    updated italian local file

--- locales/it.lua
+++ locales/it.lua
@@ -1,4 +1,10 @@
 Locales['it'] = {
     ['oRecovery'] = 'Recupero pacchetti',
     ['recoverPacket'] = 'Rcupera pacchetto',
+    ['not_enough_bmoney'] = 'Non hai abbastanza soldi!',
+    ['oxyHasStarted'] = 'Hai cominciato il lavoro.',
+    ['gpsSetted'] = 'Impostata la destinazione sul GPS.',
+    ['exchange'] = 'Punto di scambio',
+    ['no_more'] = 'Pacchetti consegnati!',
+    ['deliver_pack'] = 'Consegna pacchetto',
 }
