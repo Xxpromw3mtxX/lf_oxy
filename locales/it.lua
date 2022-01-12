fd92e22d4185319ee65993a4f5f779bf828a298a
Francesco Metta <fra.metta@hotmail.it>
2022-01-12 15:09:42 +0100

    updated italian locales

--- locales/it.lua
+++ locales/it.lua
@@ -7,4 +7,10 @@ Locales['it'] = {
     ['exchange'] = 'Punto di scambio',
     ['no_more'] = 'Pacchetti consegnati!',
     ['deliver_pack'] = 'Consegna pacchetto',
+    ['not_recovered'] = 'Non hai recuperato i pacchetti!',
+    ['no_packages'] = 'Non hai più pacchetti!',
+    ['mission_cancelled'] = 'Lavoro annullato! Ti sei allontanato troppo dalla zona di consegna.',
+    ['another_buyer'] = 'A breve riceverai la nuova posizione.',
+    ['moneyMan_coming'] = 'A breve arriverà la persona che ti porterà i soldi puliti.',
+    ['jComplted'] = 'Lavoro completato! Hai pulito %s$'
 }
