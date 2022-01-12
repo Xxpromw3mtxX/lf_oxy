5bd69baf7aaf76e1b3a0af8175bfcc23e86f0a08
Francesco Metta <fra.metta@hotmail.it>
2022-01-12 16:51:13 +0100

    added new lines

--- locales/it.lua
+++ locales/it.lua
@@ -10,8 +10,9 @@ Locales['it'] = {
     ['not_recovered'] = 'Non hai recuperato i pacchetti!',
     ['no_packages'] = 'Non hai più pacchetti!',
     ['mission_cancelled'] = 'Lavoro annullato! Ti sei allontanato troppo dalla zona di consegna.',
-    ['another_buyer'] = 'A breve riceverai la nuova posizione.',
+    ['new_position'] = 'A breve riceverai la nuova posizione.',
     ['moneyMan_coming'] = 'A breve arriverà la persona che ti porterà i soldi puliti.',
     ['jComplted'] = 'Lavoro completato! Hai pulito %s$',
     ['cooldown'] = 'Hai appena completato un lavoro. Aspetta %s secondi prima di avviarne un altro.',
+    ['bComing'] = 'Un compratore arriverà a breve.',
 }
