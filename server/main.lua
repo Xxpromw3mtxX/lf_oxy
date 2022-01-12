69b182dba147a6002bc47b6d2cbaf8a397dc641b
Francesco Metta <fra.metta@hotmail.it>
2022-01-12 15:12:03 +0100

    working server code

--- server/main.lua
+++ server/main.lua
@@ -25,6 +25,55 @@ AddEventHandler('atlantis_oxy:itemAdder', function(iName)
     xPlayer.addInventoryItem(iName, 1)
 end)
 
+--[[
+    @sPackage: package item name
+    @dOxy: oxy item name
+]]
+RegisterServerEvent('atlantis_oxy:giveOxy')
+AddEventHandler('atlantis_oxy:giveOxy', function(sPackage, dOxy)
+    local xPlayer = ESX.GetPlayerFromId(source)
+
+    local xPackage = xPlayer.getInventoryItem(sPackage).count
+    local finalOxy = math.random(1, Config.maxOxy)
+
+    if xPackage > 0 and xPackage ~= 1 then
+        xPlayer.removeInventoryItem(sPackage, 1)
+        xPlayer.addInventoryItem(dOxy, finalOxy)
+        TriggerClientEvent('atlantis_oxy:driveAway', source)
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, {type = 'inform', text = _U('another_buyer'), length = 2500})
+        TriggerClientEvent('atlantis_oxy:waitTime', source)
+    elseif xPackage == 1 then
+        xPlayer.removeInventoryItem(sPackage, 1)
+        xPlayer.addInventoryItem(dOxy, finalOxy)
+        local clearedMoney = math.floor((Config.startAmount*15)/2.05)
+        xPlayer.addAccountMoney('money', clearedMoney)
+        TriggerClientEvent('atlantis_oxy:driveAway', source)
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, {type = 'inform', text = _U('jComplted', clearedMoney), length = 2500})
+        TriggerClientEvent('atlantis_oxy:resetFlag', source)
+    else
+        TriggerClientEvent('mythic_notify:client:SendAlert', source, {type = 'error', text = _U('no_packages'), length = 2500})
+    end
+end)
+
+--[[
+    @sPackage: package item name
+    @mAccount: money account name
+    @mAmount: money amount
+    @mBack: money back
+]]
+RegisterServerEvent('atlantis_oxy:removeStash')
+AddEventHandler('atlantis_oxy:removeStash', function(sPackage, mAccount, mAmount, mBack)
+    local xPlayer = ESX.GetPlayerFromId(source)
+    local xPackage = xPlayer.getInventoryItem(sPackage).count
+
+    if mBack then
+        xPlayer.removeInventoryItem(sPackage, xPackage)
+        xPlayer.addAccountMoney(mAccount, mAmount)
+    else
+        xPlayer.removeInventoryItem(sPackage, xPackage)
+    end
+end)
+
 -- Version Checker
 PerformHttpRequest("https://raw.githubusercontent.com/xxpromw3mtxx/atlantis_oxy/main/.version", function(err, text, headers)
     Citizen.Wait(2000)
