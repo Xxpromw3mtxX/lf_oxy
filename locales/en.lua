83aa860ac3441d3f16dfe1b4689da4b64ff4576b
Francesco Metta <fra.metta@hotmail.it>
2022-01-12 09:35:12 +0100

    Added english

---
+++ locales/en.lua
@@ -0,0 +1,10 @@
+Locales['en'] = {
+    ['oRecovery'] = 'Suspicious package recovery',
+    ['recoverPacket'] = 'Retrieve package',
+    ['not_enough_bmoney'] = 'You do not have enough money!',
+    ['oxyHasStarted'] = 'You started the job.',
+    ['gpsSetted'] = 'Set the destination on the GPS.',
+    ['exchange'] = 'Exchange point',
+    ['no_more'] = 'Packages delivered!',
+    ['deliver_pack'] = 'Package delivery',
+}
