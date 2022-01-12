f6f2865e59f294e23c3ee8065c41fc661c4d9015
Francesco Metta <fra.metta@hotmail.it>
2022-01-12 15:10:45 +0100

    claned as possible

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@ Config = {}
 Config.Locale = 'it'
 
 --Wait ticktime
-Config.TickTime = 1000
+Config.TickTime = 100
 
 --Starting Black money amount
 Config.usebMoney = true
@@ -25,23 +25,16 @@ Config.npcLocations = {
         position = vector3(213.1, -3316.8, 4.8),
         heading = 184.06
     },
-    --[[{
-        position = vector3(x,y,z),
-        heading = float
-    },
-    {
-        position = vector3(x,y,z),
-        heading = float
-    }]]
 }
 
 --Suspicious package delivery
 Config.deliveryPoints = {
-    vector3(969.77990, -1557.03700, 30.20764),
+    --[[vector3(969.77990, -1557.03700, 30.20764),
     vector3(-173.59390, -1975.40200, 27.14530),
     vector3(-777.35500, 373.81520, 87.35204),
     vector3(-939.38060, 309.26740, 70.67439),
-    vector3(-227.40120, -1486.49900, 30.79996),
+    vector3(-227.40120, -1486.49900, 30.79996),]]
+    vector3(217.7, -166.3, 56.6),
 }
 
 --Ped vehicle model, drive types and max speed
@@ -53,49 +46,46 @@ Config.vehicleModels = {
     'sultan',
 }
 
-Config.vehicleDriveType = {
-    1074528293, --Rushed
-    786603, --Normal
-    2883621, --Ignore Lights
-    5, --Sometimes Overtake Traffic
-}
+Config.dType = 786603 --Normal
 
-Config.pedVehiclemSpeed = 130.0
+Config.pedVehiclemSpeed = 10.0
 
---Ped models
-Config.sellerPed = {
-    {
-        model = 'g_f_y_families_01',
-        type = 5
+--Peds
+Config.peds = {
+    sellers = {
+        {
+            model = 'g_f_y_families_01',
+            type = 5
+        },
+        {
+            model = 'g_f_y_vagos_01',
+            type = 5
+        },
+        {
+            model = 'g_m_m_chicold_01',
+            type = 4
+        },
+        {
+            model = 'g_m_y_lost_01',
+            type = 4
+        }
     },
-    {
-        model = 'g_f_y_vagos_01',
-        type = 5
-    },
-    {
-        model = 'g_m_m_chicold_01',
-        type = 4
-    },
-    {
-        model = 'g_m_y_lost_01',
-        type = 4
+
+    buyers = {
+        {
+            model = 'a_f_y_business_02',
+            type = 5
+        },
+        {
+            model = 'a_f_y_tourist_01',
+            type = 5
+        },
+        {
+            model = 'a_m_m_socenlat_01',
+            type = 4
+        },
     }
 }
 
-Config.pedModels = {
-    {
-        model = 'a_f_y_business_02',
-        type = 5
-    },
-    {
-        model = 'a_f_y_tourist_01',
-        type = 5
-    },
-    {
-        model = 'a_m_m_socenlat_01',
-        type = 4
-    },
-}
-
 --Distances
-Config.interactDistances = 2
+Config.interactDistance = 2
